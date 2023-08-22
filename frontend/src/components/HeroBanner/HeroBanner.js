3f9caeb2cdcaa1d488c89a47d2d44d582ce3e5cd
Adrian Karp <Adrian.Karp@ibm.com>
2023-08-22 17:20:02 +0300

    Hero banner

---
+++ frontend/src/components/HeroBanner/HeroBanner.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import heroBannerImage from '../../assets/hero-banner-image.jpg';
+
+const HeroBanner = () => {
+  return (
+    <div className="flex flex-col md:flex-row w-full bg-mainGray ">
+      <div className="flex flex-col w-full md:w-1/2 pr-4 p-24">
+        <h1 className="text-4xl md:text-6xl font-bold mb-4">
+          Welcome to BazaarBasket
+        </h1>
+        <p className="text-lg md:text-xl mb-4 w-full md:w-2/3">
+          Your one-stop shop for high-quality products across a variety of
+          categories. From fashion to electronics, we've got you covered.
+        </p>
+        <Link
+          to="/shop"
+          className="bg-black text-white px-2 py-1 rounded-full mb-4 w-32 text-center"
+        >
+          Shop Now
+        </Link>
+        <div className="grid grid-cols-3 gap-4 w-full md:w-2/3">
+          <div>
+            <h3 className="text-xl font-bold">200+</h3>
+            <p>International Brands</p>
+          </div>
+          <div>
+            <h3 className="text-xl font-bold">2000+</h3>
+            <p>High-Quality Products</p>
+          </div>
+          <div>
+            <h3 className="text-xl font-bold">30,000+</h3>
+            <p>Happy Customers</p>
+          </div>
+        </div>
+      </div>
+      <div className="w-full md:w-1/2 mt-4 md:mt-6 ">
+        <img
+          src={heroBannerImage}
+          alt="Your all-purpose shop"
+          className="w-full h-full object-cover md:object-top"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HeroBanner;
--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,38 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
+import HeroBanner from '../../components/HeroBanner/HeroBanner';
 
 const Home = () => {
   return (
-    <div className=" flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="container text-center mx-auto py-10 flex-grow lg:py-20">
-        <p className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-3">
-          Welcome to BazaarBasket!
-        </p>
-        <p className="text-xl lg:text-2xl mb-6">
-          Discover what we have in store for you.
-        </p>
-        <div className="mt-10 sm:mt-20 lg:mt-44 text-left px-4 sm:px-10 flex flex-col">
-          <p className="text-2xl sm:text-3xl lg:text-4xl font-bold mb-4">
-            About Us
-          </p>
-          <p className="text-lg sm:text-xl">
-            BazaarBasket is your one-stop shop for all your needs. We believe in
-            providing high-quality products at reasonable prices.
-          </p>
-          <p className="text-lg sm:text-xl">
-            Shop with us and experience the difference!
-          </p>
-          <Link
-            to="/products"
-            className="mt-10 sm:mt-20 text-sm sm:text-l bg-primary text-white rounded-full px-6 sm:px-10 py-2 sm:py-3 hover:bg-secondary transition-colors duration-300 w-48 sm:w-64 mx-auto"
-          >
-            See Available Products
-          </Link>
-        </div>
-      </div>
+      <HeroBanner />
       <Footer />
     </div>
   );
