8d9b64c2cca2865fcd9eeff389c6779e88ec43db
Adrian Karp <Adrian.Karp@ibm.com>
2023-08-31 11:32:59 +0300

    added service worker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,19 @@ import App from './App';
 import { CartProvider } from './utils/context/cartContext';
 import { ToastProvider } from 'react-toast-notifications';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered:', registration);
+      })
+      .catch((error) => {
+        console.log('Service Worker registration failed:', error);
+      });
+  });
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ToastProvider autoDismiss={true} placement="bottom-right">
